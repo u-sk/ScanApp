254d3bad96236914b1627cd5a97abeaff2531cb9
u-sk <u.eggplant@gmail.com>
2019-09-25 10:35:09 +0900

    Initial Commit

---
+++ ScanApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScanApp
+//
+//  Created by 板垣有祐 on 2019/09/25.
+//  Copyright © 2019 Yusuke Itagaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
